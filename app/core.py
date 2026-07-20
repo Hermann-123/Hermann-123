1ed943d8478c7ab987cf27633759dc3b3bdc261a
Hermann-123 <degbegnonhermann096@gmail.com>
2026-07-20 23:49:23 +0000

    Update core.py

--- app/core.py
+++ app/core.py
@@ -3,11 +3,11 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    TELEGRAM_BOT_TOKEN: str = "7432405570:AAFiRcY-KaUeA4G2-FaTQBy5D3d7hetgieI"
+    TELEGRAM_BOT_TOKEN: str = "7432405570:AAEWO1NdWvFuFTUG_bOwLHWcIi_w0_ssymk"
     ADMIN_ID: int = 5968288964
     API_KEY_ODDS: str = "55a670c7b44c3dcc3c9750e9f5c51da1"
     
-    # REMPLACEZ LE NUMÉRO CI-DESSOUS PAR L'ID DE VOTRE CANAL (N'oubliez pas le -100 au début)
+    # ⚠️ REMETTEZ LE VRAI ID DE VOTRE CANAL ICI :
     ARCHIVE_CHANNEL_ID: str = "-1003982738017" 
     
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
@@ -22,3 +22,6 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger("WallStreet_OS")
 
 CACHE_PORTFOLIO = {}
+
+# NOUVEAU : Mémoire pour sauvegarder votre Bankroll
+USER_BANKROLLS = {}
