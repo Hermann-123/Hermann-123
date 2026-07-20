9cbbbee4f4c8288ad5667d3790145235f76e9eb0
Hermann-123 <degbegnonhermann096@gmail.com>
2026-07-20 23:07:59 +0000

    Update core.py

--- app/core.py
+++ app/core.py
@@ -3,7 +3,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    TELEGRAM_BOT_TOKEN: str = "8658287331:AAGFmhAcBSoNHw7OvcNT0jaS3t6OsLpTzAw"
+    TELEGRAM_BOT_TOKEN: str = "7432405570:AAHTeoBBI4x1oMkENCy3EGim267_xgb3LQI"
     ADMIN_ID: int = 5968288964
     API_KEY_ODDS: str = "55a670c7b44c3dcc3c9750e9f5c51da1"
     
