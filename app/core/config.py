33658b6a682ca1222b765fb8d8edb7944fc8b3c6
Hermann-123 <degbegnonhermann096@gmail.com>
2026-07-20 08:31:24 +0000

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "7432405570:AAENOX3pa6pIgxoq8yQAlEq2WXEznEHjQCs")
+    TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "7432405570:AAGTHmIi7sfO-xvA58yXksQ7pNCkcH1-sUc")
     ADMIN_ID: int = 5968288964
     API_KEY_ODDS: str = "55a670c7b44c3dcc3c9750e9f5c51da1"
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
