3515712ad9c13cc92e1d7b6f4f676bf3e92dd2b7
Hermann-123 <degbegnonhermann096@gmail.com>
2026-07-20 22:22:44 +0000

    Update core.py

--- app/core.py
+++ app/core.py
@@ -4,11 +4,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str = "8658287331:AAGFmhAcBSoNHw7OvcNT0jaS3t6OsLpTzAw"
-    ADMIN_ID: int = -1003982738017
+    ADMIN_ID: int = 5968288964
     API_KEY_ODDS: str = "55a670c7b44c3dcc3c9750e9f5c51da1"
     
     # REMPLACEZ LE NUMÉRO CI-DESSOUS PAR L'ID DE VOTRE CANAL (N'oubliez pas le -100 au début)
-    ARCHIVE_CHANNEL_ID: str = "-100VOTRE_ID_ICI" 
+    ARCHIVE_CHANNEL_ID: str = "-1003982738017" 
     
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
 
