cf9c1ccbf8ae4252f2ace0239650ac446f3dffb4
Hermann-123 <degbegnonhermann096@gmail.com>
2026-07-22 00:53:11 +0000

    Update core.py

--- app/core.py
+++ app/core.py
@@ -1,3 +1,4 @@
+python
 import os
 import logging
 
@@ -8,7 +9,10 @@ class Settings:
     TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "7432405570:AAE7n5uHHju2--gpsFKOCc45UyvltdW8oTU")
     ADMIN_ID: int = int(os.getenv("ADMIN_ID", "5968288964"))
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
+    
+    # 🔑 TA NOUVELLE CLÉ API-FOOTBALL OFFICIELLE
     API_KEY_FOOTBALL: str = os.getenv("API_KEY_FOOTBALL", "99f9731b68429ed4aaf0383cd7ca8cd4")
+    
     ARCHIVE_CHANNEL_ID: str = os.getenv("ARCHIVE_CHANNEL_ID", "-1003982738017")
 
 settings = Settings()
