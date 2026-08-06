7058e5bb2fa4a6fdf46b6e63af91ee222425af24
Hermann-123 <degbegnonhermann096@gmail.com>
2026-08-06 17:24:00 +0000

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -40,10 +40,10 @@ async def command_start(message: Message):
         "⚡️ Flux The Odds API : Connecté\n"
         "⚙️ Moteur Combinatoire : Actif\n"
         "🤖 IA Groq : Connectée\n\n"
-        "Utilisez le clavier en bas pour sélectionner votre combiné :"
-    )
+        "👤 créateur : @hermann1123\n"
+        "Utilisez le clavier en bas pour sélectionner votre combiné :")
     await message.answer(text, reply_markup=main_keyboard(), parse_mode="Markdown")
-
+)
 # 📲 GESTION DES CLICS SUR LES BOUTONS DU CLAVIER
 @router.message(F.text.in_([
     "🌟 Combiné du Jour",
