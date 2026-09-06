2efee49843a6f456af57b04dbc11700edf2cc218
Hermann-123 <degbegnonhermann096@gmail.com>
2026-09-06 10:42:59 +0000

    Update Telegram bot token in Settings class

--- app/core.py
+++ app/core.py
@@ -5,7 +5,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(level
 logger = logging.getLogger("WallStreet_OS")
 
 class Settings:
-    TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "7432405570:AAFCabv7yurFDp7qmQPTLnUCP5nEoAkk248")
+    TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "7432405570:AAEtOW-lQEghcsmmWpVCvhXHI8OTZp61Y6o")
     ADMIN_ID: int = int(os.getenv("ADMIN_ID", "5968288964"))
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
     
